1917d896f0810d60abc4cb45841811f1183323b0
june29 <june29.jp@gmail.com>
2018-06-09 20:51:00 +0900

    :muscle: 各項目にチェックボックスを置いて「完 / 未完」を表現できる

--- todo-jquery/app.js
+++ todo-jquery/app.js
@@ -2,6 +2,6 @@ $(function() {
   $("input[type='button']").on("click", function () {
     var text = $("input[type='text']").val();
 
-    $("<li>").text(text).appendTo("ul");
+    $("<li><label><input type='checkbox'>" + text + "</label></li>").appendTo("ul");
   });
 });
