7e7b0d14ff7f78213e7eb604dae12bdef6c01921
june29 <june29.jp@gmail.com>
2018-06-09 21:03:57 +0900

    :sparkles: functionのあとに半角スペースは挿れない

--- todo-jquery/app.js
+++ todo-jquery/app.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("input[type='button']").on("click", function () {
+  $("input[type='button']").on("click", function() {
     var text = $("input[type='text']").val();
 
     $("<li><label><input type='checkbox'>" + text + "</label></li>").appendTo("ul");
